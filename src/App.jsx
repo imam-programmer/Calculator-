24f250bf8aec8e6bbadf87d3de396c3c777c1c55
imam-programmer <imamprogrammer09@gmail.com>
2026-07-02 15:04:11 +0600

    functionally calculator creating done

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,21 @@ import Display from './components/Display'
 import ButtonsContainer from './components/ButtonsContainer'
 const App = () => {
   const [calVal, setCalVal] = useState("")
+  function onButtonClick(buttonText){
+   if(buttonText==="C"){
+setCalVal("")
+   }else if(buttonText==="="){
+const result=eval(calVal)
+setCalVal(result)
+   }else{
+    const newDisplayValue=calVal + buttonText
+    setCalVal(newDisplayValue)
+   }
+  }
   return (
     <div className={styles.calculator} >
      <Display displayValue={calVal}/>
-      <ButtonsContainer/>
+      <ButtonsContainer onButtonClick={onButtonClick}/>
     </div>
   )
 }
--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,13 +1,36 @@
 import React from "react";
-import styles from "./ButtonsContainer.module.css"
-const ButtonsContainer = () => {
-    const buttonNames=["C","1","2","+","3","4","-","5","6","*","7","8","/","=","9","0","."]
+import styles from "./ButtonsContainer.module.css";
+const ButtonsContainer = ({ onButtonClick }) => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <div>
       <div className={styles.buttonsContainer}>
-        {buttonNames.map(buttonName=><button className={styles.button}>{buttonName}</button>)}
-        
-  
+        {buttonNames.map((buttonName) => (
+          <button key={buttonName}
+            className={styles.button}
+            onClick={() => onButtonClick(buttonName)}
+          >
+            {buttonName}
+          </button>
+        ))}
       </div>
     </div>
   );
